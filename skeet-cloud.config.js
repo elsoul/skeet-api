aee4abb38c3694da3356d4d2eb470ba4f70a403a
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-01-23 15:03:17 +0100

    add webpack

--- skeet-cloud.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('skeet').skeetCloudConfig} */
-const skeetCloudConfig = {
-  projectId: 'skeet-framework',
-  appName: 'skeet-api',
-  region: 'europe-west4',
-  cpu: '1',
-  memory: '1Gi',
-}
-module.exports = skeetCloudConfig
